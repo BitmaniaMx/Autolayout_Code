d1bdb175b3b94e1dea3f6500a13ae9358521ec57
Rafael <rafael.glez@gmail.com>
2024-08-23 18:06:33 -0600

    Initial Commit

---
+++ Autolayout_Code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Autolayout_Code
+//
+//  Created by Rafael Gonzalez on 23/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
